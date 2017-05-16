f39a58aa11f6cb8dfc3da620f472b32172b49f40
Chris Hudson <c.ahudson84@yahoo.com>
2017-05-16 14:28:45 -0700

    stuff

--- series.py
+++ series.py
@@ -1,23 +1,40 @@
+"""Doc strings everywhere."""
+
+
 def fib(n):
     """For any n, return its zero-index Fibonacci sequence."""
-    if n <= 0:
-        return 0
-    elif n == 1:
-        return 1
+    if type(n) == int:
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return fib(n - 1) + fib(n - 2)
     else:
-        return fib(n - 1) + fib(n - 2)
+        return 'Please use an integer.'
 
-def lucas(x,y,n):
-    """ finds the number in the lucas sequnce"""
-    if ( n == 1):
-        return x
-    elif ( n == 2):
-        return y
-    n = n -2
-    temp = 0
-    for z in range(n):
-        temp = x + y
-        x = y
-        y = temp
-    return temp
 
+def lucas(n):
+    """For any n, return its zero-index Fibonacci sequence."""
+    if type(n) == int:
+        if n <= 0:
+            return 2
+        elif n == 1:
+            return 1
+        else:
+            return fib(n - 1) + fib(n - 2)
+    else:
+        return 'Please use an integer.'
+
+
+def sum_series(n, x=0, y=1):
+    """Find the number in the lucas sequnce."""
+    if type(n) == int:
+        if (n == 0):
+            return x
+        elif (n == 1):
+            return y
+        else:
+            return sum_series(n - 1) + sum_series(n - 2)
+    else:
+        return 'Please use integers.'
--- series_test.py
+++ series_test.py
@@ -15,23 +15,42 @@ FIB_PARAMS = [
 
 
 LUCAS_PARAMS = [
-    (0, 1, 1, 0),
-    (3 ,2 , 5, 12),
-    (5,8,4,21),
-    (9,7,6,62),
-    (9,2,5,24)
+    (0, 2),
+    (1, 1),
+    (6, 18),
+    (3, 4),
+    (8, 47)
+]
+
+
+SUM_PARAMS = [
+    (4, 3),
+    (5, 5),
+    (0, 0),
+    (1, 0, 1, 0),
+    (5, 3, 2, 12),
+    (4, 5, 8, 21),
+    (6, 9, 7, 62),
+    (5, 9, 2, 24)
 ]
 
 
 @pytest.mark.parametrize('n, result', FIB_PARAMS)
-def fib_test(n):
-    """Tests to see if the Fibonacci nonsense returns the appropriate value."""
+def fib_test(n, result):
+    """Test to see if the Fibonacci nonsense returns the appropriate value."""
     from fib import fib
-    assert fib(n) = result
+    assert fib(n) == result
 
-@pytest.mark.parametrize('x,y,n,result',LUCA_PARMS)
-def lucas_test(x,y,z):
-    """Test the lucas function"""
+
+@pytest.mark.parametrize('n, result', LUCAS_PARAMS)
+def lucas_test(n, result):
+    """Test to see if the Lucas nonsense returns the appropriate value."""
     from fib import lucas
-    assert lucas(x,y,n) =result
+    assert lucas(n) == result
 
+
+@pytest.mark.parametrize('n,result,x,y', SUM_PARAMS)
+def sum_series_test(n, result, x=0, y=1):
+    """Test the sum_series function."""
+    from fib import sum_series
+    assert sum_series(n, x, y) == result
